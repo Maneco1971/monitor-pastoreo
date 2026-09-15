4ce3be0bece16276de0c97dfeccc22cb193627af
Maneco1971 <jsoaresdelima@inia.org.uy>
2026-09-15 09:36:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,8 @@ st.title("🌾 Monitor de Pastoreo y Estado de Parcelas")
 
 # Parámetro de conexión a Google Sheets (Identificador de la planilla DB_Pastoreo)
 SHEET_ID = "1uDFTp_B8NMuu4vteXgAY_ACiStijAr6UZdC6bYSCVgA"
-    
-    url_movimientos = f"https://docs.google.com/spreadsheets/d/1uDFTp_B8NMuu4vteXgAY_ACiStijAr6UZdC6bYSCVgA/gviz/tq?tqx=out:csv&sheet=Movimientos"
-    url_lotes = f"https://docs.google.com/spreadsheets/d/1uDFTp_B8NMuu4vteXgAY_ACiStijAr6UZdC6bYSCVgA/gviz/tq?tqx=out:csv&sheet=Lotes"
+url_movimientos = f"https://docs.google.com/spreadsheets/d/1uDFTp_B8NMuu4vteXgAY_ACiStijAr6UZdC6bYSCVgA/gviz/tq?tqx=out:csv&sheet=Movimientos"
+url_lotes = f"https://docs.google.com/spreadsheets/d/1uDFTp_B8NMuu4vteXgAY_ACiStijAr6UZdC6bYSCVgA/gviz/tq?tqx=out:csv&sheet=Lotes"
 
 @st.cache_data(ttl=300)
 def cargar_datos():
